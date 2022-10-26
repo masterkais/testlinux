b4d30fb6976aef56481b5ee651580b822fae044f
masterkais <kais2032@gmail.com>
2022-10-26 16:33:08 +0100

    ajout entéte

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+############################################################
+#
+#  Description : déploiement à la volée de conteneur docker
+#
+#  Auteur : Kais trabelsi
+#
+#  Date : 28/12/2022
+#
+###########################################################
+
+
+echo "
+
+Options :
+                - --create : lancer des conteneurs
+
+                - --drop : supprimer les conteneurs créer par le deploy.sh
+
+                - --infos : caractéristiques des conteneurs (ip, nom, user...)
+
+                - --start : redémarrage des conteneurs
+
+                - --ansible : déploiement arborescence ansible
+
+"
+
