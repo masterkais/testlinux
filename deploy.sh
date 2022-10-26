182947fa16ecf40e4c4521dd042300fec0180473
masterkais <kais2032@gmail.com>
2022-10-26 16:54:20 +0100

    [DEV] : mise en place des condition des arguments

--- deploy.sh
+++ deploy.sh
@@ -4,25 +4,63 @@
 #
 #  Description : déploiement à la volée de conteneur docker
 #
-#  Auteur : Kais trabelsi
+#  Auteur : Xavier
 #
-#  Date : 28/12/2022
+#  Date : 28/12/2018
 #
 ###########################################################
 
+#si option --create
+if [ "$1" == "--create" ];then
+  echo ""
+	echo " notre option est --create"
+	echo ""
+
+# si option --drop
+elif [ "$1" == "--drop" ];then
+
+  echo ""
+	echo " notre option est --drop"
+	echo ""
+
+# si option --start
+elif [ "$1" == "--start" ];then
+  
+  echo ""
+	echo " notre option est --start"
+	echo ""
+
+# si option --ansible
+elif [ "$1" == "--ansible" ];then
+  
+  echo ""
+	echo " notre option est --ansible"
+	echo ""
+
+# si option --infos
+elif [ "$1" == "--infos" ];then
+  
+  echo ""
+	echo " notre option est --infos"
+	echo ""
+
+# si aucune option affichage de l'aide"
+else 
 
 echo "
 
 Options :
-                - --create : lancer des conteneurs
+		- --create : lancer des conteneurs
 
-                - --drop : supprimer les conteneurs créer par le deploy.sh
+		- --drop : supprimer les conteneurs créer par le deploy.sh
 
-                - --infos : caractéristiques des conteneurs (ip, nom, user...)
+		- --infos : caractéristiques des conteneurs (ip, nom, user...)
 
-                - --start : redémarrage des conteneurs
+		- --start : redémarrage des conteneurs
 
-                - --ansible : déploiement arborescence ansible
+		- --ansible : déploiement arborescence ansible
 
 "
+
+fi
 
